4c31fd2d8530bb72f33f2f1bed623d6d5b262e51
MD Limon <fhlimon360@gmail.com>
2025-07-14 14:44:53 +0600

    added dragon-news-project link

--- src/Layout/FindUs/FindUs.jsx
+++ src/Layout/FindUs/FindUs.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const FindUs = () => {
     return (
         <div className='flex flex-col gap-3'>
-            <button className="btn hover:bg-blue-500"><FaFacebook></FaFacebook> Facebook</button>
+            <Link to='https://www.facebook.com/md.limon.280500' className="btn hover:bg-blue-500"><FaFacebook></FaFacebook>Facebook</Link>
             <button className="btn hover:bg-blue-500"><FaTwitter></FaTwitter>Twitter</button>
             <button className="btn hover:bg-blue-500"><FaInstagram></FaInstagram>Instagram</button>
         </div>
